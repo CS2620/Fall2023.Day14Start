d1e79ea9ca536f4b39fdbc19da0417bd2d9dfcff
B. Ricks <bricks@unomaha.edu>
2023-11-08 11:42:52 -0600

    Revert to before class

--- color_conversion.py
+++ color_conversion.py
@@ -5,11 +5,8 @@ def rgb_to_hsv(r, g, b):
   return (h, s, v)
 
 def rgb_to_cmyk(r, g, b):
-  mx = max(r/255,g/255,b/255)
-  k = 1 - mx
-  if mx == 0:
-    return (0,0,0,1)
-  c = (1-r/255-k)/(1-k)
-  m = (1-g/255-k)/(1-k)
-  y = (1-b/255-k)/(1-k)
+  k = 0
+  c = 0
+  m = 0
+  y = 0
   return (int(c*255),int(m*255),int(y*255),int(k*255))
--- color_conversion_start.py
+++ color_conversion_start.py
@@ -5,8 +5,11 @@ def rgb_to_hsv(r, g, b):
   return (h, s, v)
 
 def rgb_to_cmyk(r, g, b):
-  c = 0
-  m = 0
-  y = 0
-  k = 0
-  return (c,m,y,k)
+  mx = max(r/255,g/255,b/255)
+  k = 1 - mx
+  if mx == 0:
+    return (0,0,0,1)
+  c = (1-r/255-k)/(1-k)
+  m = (1-g/255-k)/(1-k)
+  y = (1-b/255-k)/(1-k)
+  return (int(c*255),int(m*255),int(y*255),int(k*255))
